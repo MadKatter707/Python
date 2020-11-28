b26af227d8b7a2b40f065675119c1ebac9384a90
人間失格 <58777619+MadKatter707@users.noreply.github.com>
2020-11-28 23:52:26 +0100

    Add files via upload

---
+++ 03 Variables & data types.py
@@ -0,0 +1,35 @@
+print("There once was a man named George, ")
+print("he was 70 years old.")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
+#here comes the talk about manually changing names, for example
+print("There once was a man named John, ")
+print("he was 70 years old.")
+print("He really liked the name John, ")
+print("but didn't like being 70.")
+#blablabla let's make him half his age
+print("There once was a man named John, ")
+print("he was 35 years old.")
+print("He really liked the name John, ")
+print("but didn't like being 35.")
+#blablabla don't do this, use variables was the point of this whole 5min talk
+#changing code
+character_name = "John"
+character_age = "35"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + character_age + ".")
+#nyanya so now you can just change one line of code and not many others ;D
+#let's change name halfway of this small story
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + character_age + ".")
+character_age = 50.5678213
+isMale = True
+isFemale = False
+#when storing a number there is no need for quotation marks
+#boolean values have capital T/F
+#these are the basic types of data, he didn't mention other
