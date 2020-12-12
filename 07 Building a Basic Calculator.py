4a4b51bc1f2c70bdf2491ac7257d1070beddc31c
人間失格 <58777619+MadKatter707@users.noreply.github.com>
2020-12-12 22:50:07 +0100

    Add files via upload

---
+++ 07 Building a Basic Calculator.py
@@ -0,0 +1,19 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = num1 + num2
+print(result)
+#run
+#Enter a number: 4
+#Enter another number: 5
+#45
+
+#solution
+result1 = int(num1) + int(num2)
+print(result1)
+#there is another problem here, and that is decimal numbers
+#solution
+num3 = input("Enter a float number: ")
+num4 = input("Enter another float number:")
+result2 = float(num3) + float(num4)
+print(result2)
+#calculator xDDDDDDDDDD
