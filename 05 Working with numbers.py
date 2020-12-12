6ba258d288a3b7249766f1a7d8cf7d669a9d7d85
人間失格 <58777619+MadKatter707@users.noreply.github.com>
2020-12-12 22:14:45 +0100

    Add files via upload

---
+++ 05 Working with numbers.py
@@ -0,0 +1,42 @@
+from math import *
+print(2)
+print(2.0987)
+print(-2.0987)
+print(3+4)
+print(3+4.5)
+print(3*4.5)
+print(3/4.5)
+print(3-4.5)
+print(3*4+5)
+print(3*(4+5))
+print(10 % 3)
+my_num = 7
+print(my_num)
+print(str(my_num)+" my favorite number" )
+#print(my_num +" my favorite number" )
+#Traceback (most recent call last):
+# File "C:\Users\lololololstreamIncepetionbyAteez\PycharmProjects\Giraffe\05 Working with numbers.py", line 15, in <module>
+#    print(my_num +" my favorite number" )
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+#time for San's choco abs
+Sans_num = -9
+print(abs(Sans_num))
+#time for power of Choi San
+print(pow(2, Sans_num)) #i got confusion but it's cuz san's number is negative ahahah
+print(pow(2, my_num))
+print(pow(2, 5)) #2^5 savvy?
+print(max(my_num, Sans_num)) #max number
+print(max(-12, Sans_num, -23))
+print(min(my_num, Sans_num))
+print(min(1, 3 , 4, 65))
+eight_makes_one_team = 8
+print(eight_makes_one_team)#this was for funs xD
+print(round(pow(2, Sans_num)))
+print(round(4.566))
+print(round(pow(Sans_num, my_num)))
+#importing math at the beginning for next lines
+print(floor(3.7))
+print(ceil(3.8))
+print(sqrt(9))
+#Hearts Awakened Live Alive
