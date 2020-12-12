96d8bd65dc31dce93b5279a7fc36dcec38805374
人間失格 <58777619+MadKatter707@users.noreply.github.com>
2020-12-12 22:34:56 +0100

    Add files via upload

---
+++ 06 Getting Input From Users.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("Hello " + name + "!")
+#run (i typed rum to many times... why is the rum always gone?)
+#C:\Users\ateeeeeeeeez\PycharmProjects\Giraffe\venv\Scripts\python.exe "C:/Users/katar/PycharmProjects/Giraffe/06 Getting Input From Users.py"
+#Enter your name: Choi San
+#Hello Choi San!
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
+#Enter your age: 22
+#Hello Choi San! You are 22
+#SO PLEAAAAAAAAAAAAASEEEEEEEEEEEEE ITS ALRIGHTTTTTT (BEAR WITH ME TILL I LEARN KOREAN)
