06efb19fc7cfb6482e0082df519e7ead784d894c
人間失格 <58777619+MadKatter707@users.noreply.github.com>
2020-12-15 04:41:24 +0100

    Add files via upload

---
+++ 08 Mad Libs Game.py
@@ -0,0 +1,19 @@
+print("Roses are red")
+print("Violets are blue")
+print("Unexpected ; on line 32")
+print("And joke's on you")
+print("This is python ;D")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+#print("Roses are {color}")
+#print("{plural noun} are blue")
+#print("I love {celebrity}")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+
+#more i believe in this dude's words the more i am disappointed
